64a3b1e91fa600dce57deb346a2b2d5c5944ddff
jjanmo <jjanmo07@hanmail.net>
2023-01-09 00:36:30 +0900

    fix: fix auto-off issue

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-
+options = Options()
+options.add_experimental_option("detach", True)  # prevent auto-Off
+driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
 driver.get('https://google.com')
