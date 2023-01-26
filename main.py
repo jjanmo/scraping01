4195d502e04818aa6b41e534f106053720a2a0c9
jjanmo <jjanmo07@hanmail.net>
2023-01-27 00:17:45 +0900

    feat: how to search elements, how to wait specific element when exposed, element screenshots

--- main.py
+++ main.py
@@ -4,21 +4,33 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
+KEYWORD = 'buy domain'
 options = Options()
 options.add_experimental_option("detach", True)  # prevent auto-Off
-driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
-driver.get('https://google.com')
+browser = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+browser.get('https://google.com')
 
-search_bar = driver.find_element(By.CLASS_NAME, 'gLFyf')
-search_bar.send_keys("스쿨라이브")
+search_bar = browser.find_element(By.CLASS_NAME, 'gLFyf')  # select google input
+search_bar.send_keys(KEYWORD)
 search_bar.send_keys(Keys.ENTER)
 
-search_results = driver.find_elements(By.CLASS_NAME, 'LC20lb')
-results = []
-for elem in search_results:
-    results.append(elem.text)
+except_element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'cUnQKe')))
+browser.execute_script(
+    'const element = arguments[0]; element.parentElement.remove();',
+    except_element
+)
+except_element = browser.find_element(By.CLASS_NAME, 'EyBRub')
+browser.execute_script(
+    'const element = arguments[0]; element.parentElement.remove();',
+    except_element
+)
 
-print(results)
+results = browser.find_elements(By.CSS_SELECTOR, '#search .MjjYud')
+for index, result in enumerate(results):
+    result.screenshot(f'screenshots/{KEYWORD}_{index}.png')
+
 
 # driver.quit()
