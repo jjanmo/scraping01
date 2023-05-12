6e0cb985ae768a201c18434c5e90b45ae61e23ca
jjanmo <jjanmo07@hanmail.net>
2023-05-13 01:19:20 +0900

    chores

--- README.md
+++ README.md
@@ -11,3 +11,7 @@
 - Responsive Tester
 
   > 특정 웹사이트로 진입하여 width에 따라서 스크롤다운하면서 영역별로 캡쳐한다. 캡쳐된 이미지를 통해서 반응형 UI의 상태를 체크할 수 있다.
+
+## 추가 공부 자료
+
+- [나도코딩의 스크래핑](https://www.youtube.com/watch?v=yQ20jZwDjTE)
--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from common.browser import Browser
 from features.google_searching_screenshot import GoogleSearchingScreenshot
 from features.responsive_tester import ResponsiveTester
 
-google_browser = Browser('https://www.google.com').get_browser()
+google_browser = Browser("https://www.google.com").get_browser()
 
 # study_searcher = GoogleSearchingScreenshot(google_browser, 'how to learn python', 'screenshots')
 # study_searcher.get_screenshots()
 
-ts_searcher = GoogleSearchingScreenshot(google_browser, 'typescript')
-ts_searcher.get_screenshots()
+# ts_searcher = GoogleSearchingScreenshot(google_browser, 'typescript')
+# ts_searcher.get_screenshots()
 
-nomad_browser = Browser('https://nomadcoders.co').get_browser()
-ui_tester = ResponsiveTester(nomad_browser, 'https://nomadcoders.co')
+nomad_browser = Browser("https://nomadcoders.co").get_browser()
+ui_tester = ResponsiveTester(nomad_browser, "https://nomadcoders.co")
 ui_tester.make_screenshots()
